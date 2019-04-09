f7b691ea8b66c620663ef840e3ca4efa4276166a
alinegn <1483588183@qq.com>
2019-04-09 20:42:18 +0800

    '190409'

--- app/src/main/java/com/lizxing/daily/ui/English/EnglishActivity.java
+++ app/src/main/java/com/lizxing/daily/ui/English/EnglishActivity.java
@@ -8,7 +8,9 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +24,8 @@ import com.lizxing.daily.ui.MainActivity;
 import com.lizxing.daily.utils.HttpUtil;
 import com.lizxing.daily.utils.StatusBarUtil;
 import com.lizxing.daily.utils.Utility;
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
 
 import java.io.IOException;
 
@@ -34,6 +38,9 @@ public class EnglishActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TextView textView;
     private CircularProgressButton finishBtn;
+    private TickerView numTtickerView;
+    private TickerView gradeTickerView;
+    private TickerView expTickerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,9 @@ public class EnglishActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         textView = findViewById(R.id.English_text);
         finishBtn = findViewById(R.id.btnFinish);
+        numTtickerView = findViewById(R.id.tickerView_num);
+        gradeTickerView = findViewById(R.id.tickerView_grade);
+        expTickerView = findViewById(R.id.tickerView_exp);
     }
 
     private void initData(){
@@ -73,6 +83,19 @@ public class EnglishActivity extends AppCompatActivity {
             requestEnglish();
         }
 
+        //数字
+        numTtickerView.setCharacterLists(TickerUtils.provideNumberList());
+        gradeTickerView.setCharacterLists(TickerUtils.provideNumberList());
+        expTickerView.setCharacterLists(TickerUtils.provideNumberList());
+        numTtickerView.setAnimationDuration(500);
+        numTtickerView.setAnimationInterpolator(new OvershootInterpolator());
+        numTtickerView.setGravity(Gravity.START);
+        int number = prefs.getInt("number",0);
+        numTtickerView.setText(String.valueOf(number));
+        int exp = prefs.getInt("exp",0);
+        expTickerView.setText(String.valueOf(exp));
+        gradeTickerView.setText(String.valueOf(exp / 100));
+
 
         //按钮
         finishBtn.setText(getResources().getString(R.string.Finish));
@@ -92,11 +115,29 @@ public class EnglishActivity extends AppCompatActivity {
                         public void onAnimationUpdate(ValueAnimator valueAnimator) {
                             int value = (int) valueAnimator.getAnimatedValue();
                             btn.setProgress(value);
+                            if(value == 100){
+                                //更新数字
+                                int number = prefs.getInt("number",0);
+                                int exp = prefs.getInt("exp",0);
+                                ++number;
+                                if (text != null) {
+                                    exp += text.length();//句子长度决定经验值
+                                }
+                                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(EnglishActivity.this).edit();
+                                editor.putInt("number", number);
+                                editor.putInt("exp", exp);
+                                editor.apply();
+                                numTtickerView.setText(String.valueOf(number));
+                                expTickerView.setText(String.valueOf(exp));
+                                gradeTickerView.setText(String.valueOf(exp / 100));
+                            }
                         }
                     });
                     valueAnimator.start();
                 } else {
+                    //进度完成
                     btn.setProgress(0);
+                    //更新句子
                     requestEnglish();
                 }
             }
